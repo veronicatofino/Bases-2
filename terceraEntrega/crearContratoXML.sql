5cec42db8c2856028a012b050cca94a9083a3c19
veronicatofino <veronicatofino@javerianacali.edu.co>
2019-10-31 00:10:59 -0500

    Add files via upload

--- terceraEntrega/crearContratoXML.sql
+++ terceraEntrega/crearContratoXML.sql
@@ -1,6 +1,17 @@
-CREATE DIRECTORY OUR_DIR AS '/alumno/alumno2';
+
         DROP TABLE contrato_xml;
         CREATE TABLE contrato_xml OF XMLType;
+        drop table contrato_temp;
+create table CONTRATO_TEMP (fecha_Contrato DATE, fecha_Corte DATE, id_Sucursal number, numero_Documento number
+            ,tipo_Documento varchar2(15 BYTE), primer_Nombre varchar2(15 BYTE), segundo_Nombre varchar2(15 BYTE), primer_Apellido varchar2(15 BYTE), segundo_Apellido varchar2(15 BYTE)
+            ,direccion varchar2(20 BYTE), fecha_Nacimiento date, sexo varchar2(15 BYTE), ocupacion varchar2(30 BYTE), rango_Salarial_Menor number, rango_Salarial_Mayor number
+            ,telefono number, correo varchar2(30 BYTE), nombre_Ciudad varchar2(15 BYTE)
+        , id_CatxPlan number);
+        
+        
+        CREATE DIRECTORY OUR_DIR2 AS '/alumno/alumno2';
+        
+
 create or replace PROCEDURE CREAR_CONTRATO_DESDE_XML
     IS
         fechaContrato DATE;
@@ -23,30 +34,69 @@ create or replace PROCEDURE CREAR_CONTRATO_DESDE_XML
         correo VARCHAR(30 BYTE);
         nombreCiudad VARCHAR(15 BYTE);
     BEGIN
+        INSERT INTO contrato_xml VALUES (XMLType(bfilename('OUR_DIR2','contratos.xml'),nls_charset_id('AL32UTF8')));
+        INSERT INTO contrato_temp(fecha_Contrato, fecha_Corte, id_Sucursal, numero_Documento
+        ,tipo_Documento,primer_Nombre,segundo_Nombre,primer_Apellido,segundo_Apellido,direccion
+        ,fecha_Nacimiento,sexo,ocupacion,rango_Salarial_Menor,rango_Salarial_Mayor,telefono
+        ,correo,nombre_Ciudad
+        , id_CatxPlan)
+        
+        SELECT TO_DATE(z.fecha_Contrato, 'DD/MM/YYYY'), TO_DATE(z.fecha_Corte, 'DD/MM/YYYY'), z.id_Sucursal,z.numDoc
+        ,z.tipo_Documento,z.primer_Nombre,z.segundo_Nombre,z.primer_Apellido,z.segundo_Apellido,z.direccion
+        ,TO_DATE(z.fecha_Nacimiento, 'DD/MM/YYYY'),z.sexo,z.ocupacion,z.rango_Salarial_Menor,z.rango_Salarial_Mayor,z.telefono
+        ,z.correo,z.nombre_Ciudad
+        , z.id_CatxPlan from contrato_xml, 
+        XMLTABLE('/Contratos'
+      PASSING OBJECT_VALUE
+      COLUMNS 
+        contrato xmltype PATH '/Contratos/Contrato') x
+      , XMLTABLE('/Contrato'
+        PASSING x.contrato
+        columns
+          fecha_Contrato varchar(100) path '/Contrato/fecha_Contrato'
+          , fecha_Corte varchar(100) path '/Contrato/fecha_Corte'
+          , id_Sucursal number path '/Contrato/id_Sucursal'
+          , numDoc number path '/Contrato/Cliente/numero_Documento'
+          
+          , tipo_Documento varchar(100) path '/Contrato/Cliente/tipo_Documento'
+          , primer_Nombre varchar(100) path '/Contrato/Cliente/primer_Nombre'
+          , segundo_Nombre varchar(100) path '/Contrato/Cliente/segundo_Nombre'
+          , primer_Apellido varchar(100) path '/Contrato/Cliente/primer_Apellido'
+          , segundo_Apellido varchar(100) path '/Contrato/Cliente/segundo_Apellido'
+          , direccion varchar(100) path '/Contrato/Cliente/direccion'
+          , fecha_Nacimiento varchar(100) path '/Contrato/Cliente/fecha_Nacimiento'
+          , sexo varchar(100) path '/Contrato/Cliente/sexo'
+          , ocupacion varchar(100) path '/Contrato/Cliente/ocupacion'
+          , rango_Salarial_Menor number path '/Contrato/Cliente/rango_Salarial_Menor'
+          , rango_Salarial_Mayor number path '/Contrato/Cliente/rango_Salarial_Mayor'
+          , telefono number path '/Contrato/Cliente/telefono'
+          , correo varchar(100) path '/Contrato/Cliente/correo'
+          , nombre_Ciudad varchar(100) path '/Contrato/Cliente/nombre_Ciudad'
+          
+          , id_CatxPlan number path '/Contrato/id_CatxPlan') z;
+          COMPLETAR_REGISTRO_CONTRATO();
+    COMMIT;
 
-        INSERT INTO contrato_xml VALUES (XMLType(bfilename('OUR_DIR','contratos.xml'),nls_charset_id('AL32UTF8')));
-
-        SELECT TO_NUMBER(extractValue(OBJECT_VALUE,'/Contrato/Cliente/numero_Documento')) , TO_DATE(extractValue(OBJECT_VALUE,'/Contrato/fecha_Contrato'),'DD/MM/YY'),
-               TO_DATE(extractValue(OBJECT_VALUE,'/Contrato/fecha_Corte'),'DD/MM/YY'), TO_NUMBER(extractValue(OBJECT_VALUE,'/Contrato/id_Sucursal')), 
-               TO_NUMBER(extractValue(OBJECT_VALUE,'/Contrato/id_CatxPlan'))
-        INTO nDocumento , fechaContrato, fechaCorte, idSucursal, idCatxPlan
-        FROM contrato_xml;
-
-        IF VALIDAR_CLIENTE(nDocumento) THEN
-            CREAR_CONTRATO(fechaContrato,fechaCorte,idSucursal,nDocumento,idCatxPlan);
-        ELSE
-            SELECT extractValue(OBJECT_VALUE,'/Contrato/Cliente/tipo_Documento'),extractValue(OBJECT_VALUE,'/Contrato/Cliente/primer_Nombre'),extractValue(OBJECT_VALUE,'/Contrato/Cliente/segundo_Nombre'),
-                extractValue(OBJECT_VALUE,'/Contrato/Cliente/primer_Apellido'), extractValue(OBJECT_VALUE,'/Contrato/Cliente/segundo_Apellido'), extractValue(OBJECT_VALUE,'/Contrato/Cliente/direccion'), TO_DATE(extractValue(OBJECT_VALUE,'/Contrato/Cliente/fecha_Nacimiento'),'DD/MM/YY'),
-                extractValue(OBJECT_VALUE,'/Contrato/Cliente/sexo'), extractValue(OBJECT_VALUE,'/Contrato/Cliente/ocupacion'), TO_NUMBER(extractValue(OBJECT_VALUE,'/Contrato/Cliente/rango_Salarial_Menor')),
-                TO_NUMBER(extractValue(OBJECT_VALUE,'/Contrato/Cliente/rango_Salarial_Mayor')), TO_NUMBER(extractValue(OBJECT_VALUE,'/Contrato/Cliente/telefono')),extractValue(OBJECT_VALUE,'/Contrato/Cliente/correo'),
-                extractValue(OBJECT_VALUE,'/Contrato/Cliente/nombre_Ciudad')
-                INTO
-                    tipoDocumento, primerNombre, segundoNombre, primerApellido, segundoApellido, direccion, fechaNacimiento, sexo, ocupacion, rangoSalarial_Menor, rangoSalarial_Mayor, telefonoCliente,
-                    correo, nombreCiudad
-                FROM contrato_xml;
-            REGISTRAR_CLIENTE(nDocumento,tipoDocumento,primerNombre,segundoNombre,primerApellido,segundoApellido,direccion,fechaNacimiento, sexo, ocupacion, rangoSalarial_Menor, rangoSalarial_Mayor,telefonoCliente, correo, nombreCiudad);
-            CREAR_CONTRATO(fechaContrato,fechaCorte,idSucursal,nDocumento,idCatxPlan);
-                    
-        end if;
 end;
 
+
+CREATE OR REPLACE PROCEDURE COMPLETAR_REGISTRO_CONTRATO
+IS 
+     CURSOR c1 IS SELECT fecha_Contrato , fecha_Corte , id_Sucursal
+     , numero_Documento    
+            ,tipo_Documento,primer_Nombre,segundo_Nombre,primer_Apellido,segundo_Apellido,direccion
+            ,fecha_Nacimiento,sexo,ocupacion,rango_Salarial_Menor,rango_Salarial_Mayor,telefono
+            ,correo,nombre_Ciudad
+     , id_CatxPlan  FROM CONTRATO_TEMP;
+BEGIN
+    for r in c1 LOOP
+        IF VALIDAR_CLIENTE(r.numero_Documento) THEN
+            CREAR_CONTRATO(r.fecha_Contrato,r.fecha_Corte,r.id_Sucursal,r.numero_Documento,r.id_CatxPlan);
+        ELSE
+            REGISTRAR_CLIENTE(r.numero_Documento,r.tipo_Documento,r.primer_Nombre,r.segundo_Nombre,r.primer_Apellido,r.segundo_Apellido,r.direccion,r.fecha_Nacimiento, r.sexo, r.ocupacion, r.rango_Salarial_Menor, r.rango_Salarial_Mayor,r.telefono, r.correo, r.nombre_Ciudad);
+            CREAR_CONTRATO(r.fecha_Contrato,r.fecha_Corte,r.id_Sucursal,r.numero_Documento,r.id_CatxPlan);
+        COMMIT;            
+        end if; 
+        
+  END LOOP;
+END;
